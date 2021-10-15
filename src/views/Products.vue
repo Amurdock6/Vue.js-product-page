436d2cdeb30ee0dfc5776650072d86485d8be23d
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-10-14 23:47:51 -0600

    Vue.js dynamic web store

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,35 +1,25 @@
 <template>
-  <div class="home">
-          <div class="splash-container">
-        <div class="splash">
-          <h1>Splendid Food</h1>
-        </div>
-      </div>
+    <main class="wrapper">
+        <h1>Products</h1>
+        <div class="card-container">
 
-      <main class="wrapper">
-
-        <h2>Recommended</h2>
-
-        <div class="recommended">
             <ProductCard
-              v-for="(product, index) in inventory.slice(0,3)"
+              v-for="(product, index) in inventory.slice(0,6)"
               :key="product.id"
               class="card"
               :index="index"
               :product="product"
               :addToCart="addToCart"
             />
-        </div>
 
-      </main>
-  </div>
+        </div>
+    </main>
 </template>
 
 <script>
 import ProductCard from '@/components/ProductCard.vue'
 
 export default {
-  name: 'Home',
   props: ['inventory', 'addToCart'],
   components: {
     ProductCard
